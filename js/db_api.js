a1cedf057fe9e01257d63753202d8a1e495b83df
smokfyz <ivansharapenkov@gmail.com>
2018-02-03 16:47:00 +0300

    update func for interaction with db

--- js/db_api.js
+++ js/db_api.js
@@ -1,12 +1,29 @@
 const db_api = {
 
     add: function(data) {
-        const db = window.localStorage;
-        let redexp_data = JSON.parse(db.getItem('redexp_data'));
-        if(!redexp_data) redexp_data = [];
-        redexp_data.push(data);
-        db.setItem('redexp_data', JSON.stringify(redexp_data));
-        return 1;
+        const xhr = new XMLHttpRequest();
+        xhr.open('POST', 'http://localhost:3000/transactions', true);
+        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        let body = '';
+        for(let key in data) {
+            body += '&' + key + '=' + encodeURIComponent(data[key]);
+        }
+        body = body.substring(1, body.length);
+
+        xhr.onload = function() {
+            alert( JSON.parse(this.responseText) );
+        }
+
+        xhr.onerror = function() {
+            alert( 'Ошибка ' + this.status );
+        }
+
+        xhr.send(body);
+    },
+
+    get: function() {
+        const xhr = new XMLHttpRequest();
+        xhr.open('GET', 'http://localhost:3000/transactions', true);
     }
 
 }
